8c9be0fa9a37019dc4bc81aecdf770f3f2ca5520
dbo99 <oconnor.david.b@gmail.com>
2018-08-15 18:57:12 -0700

    Add files via upload

--- libs.r
+++ libs.r
@@ -1,5 +1,5 @@
 ## these will need a one-time install first - code block at bottom - then nothing more required here
-library(data.table) #speedy joins, good for derived timeseries
+library(data.table) #speedy joins & operations, good for derived timeseries
 library(tidyverse) # gets dplyr, ggplot2, readr, lubridate -- install by pasting install.packages("tidyverse") into console  (https://www.tidyverse.org/ - wonderful libraries!)
 library(magrittr)  # %<>% pipes!  
 library(zoo)       # used for mon-years in timeseries plots  
