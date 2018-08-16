05474ee9462a6ba9d7d0be10dca626d30353dbec
dbo99 <oconnor.david.b@gmail.com>
2018-08-15 19:02:25 -0700

    Update libs.r

--- libs.r
+++ libs.r
@@ -5,7 +5,7 @@ library(magrittr)  # %<>% pipes!
 library(zoo)       # used for mon-years in timeseries plots  
 library(plotly)    # ggplotly() converts ggplot to interactive html - good with lots of scenarios as colors become hard to distinguish
 library(lubridate) # for easy dates #should be in already as part of tidyverse, but seems sometimes needed for `month` function being unrecognized w/o it (zoo interference?)
-library(cowplot)   # not used in core files, but makes easy stacking plots, with plot_grid, eg plot <- plot_grid(p1, p2, ncol = 1) #or nrow = 2, or both (2x2), good default plot aesthetics
+library(cowplot)   # not used in core files, but makes easy stacking plots with `plot_grid`, eg plot <- plot_grid(p1, p2, ncol = 1) #or nrow = 2, or both (2x2), good default plot aesthetics
 library(ggridges)  # ridge plots!
 library(viridis)   # nice color scale, robust to colorblindness
 library(RColorBrewer) # more color stuff
